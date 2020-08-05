0c531b85cde0455efcd9038da74ed1c177ae9c18
hoekma <mark@processmetric.com>
2020-08-04 20:22:42 -0600

    Bump RedPop version, randomize test event data

--- src/publishTestEvents.js
+++ src/publishTestEvents.js
@@ -4,10 +4,18 @@ const config = require('./config');
 const publisher = new Publisher(config);
 
 const eventsToPublish = 1000000;
+const messageToPublish = {
+  string: faker.random.word(),
+  date: faker.random.date(),
+  number: faker.random.number(),
+  boolean: true,
+  stringArray: [faker.random.word(), faker.random.word(), faker.random.word()],
+  object: { word: faker.random.word(), number: faker.random.word() },
+};
 
 (async () => {
   for (let i = 0; i < eventsToPublish; i++) {
-    const eventId = await publisher.publish({ message: faker.random.word });
+    const eventId = await publisher.publish(messageToPublish);
     console.log(`Published Event ${eventId}`);
   }
   process.exit();
